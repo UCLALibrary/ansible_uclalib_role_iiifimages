31d37cc204c97cc194699aa7d71f218f281a876b
Anthony Vuong <avuong@library.ucla.edu>
2016-09-20 14:40:49 -0700

    add github branch functionality

--- files/trigger-jenkins.sh
+++ files/trigger-jenkins.sh
@@ -22,10 +22,11 @@ HOST_NAME=$3
 SOLR_SERVER="http%3A%2F%2Ft-w-solrlb01.library.ucla.edu%2Fsolr%2Fjiiify"
 RAM="1024m"
 JKS_PATH="/etc/jiiify/jiiify.jks"
+GITHUB_BRANCH=$4
 #fi
 
 # Start the build
-curl -u "ksclarke:${1}" -L -s "${JOB_URL}/buildWithParameters?delay=0sec&HOST=${HOST_NAME}&JKS=${JKS_PATH}&SOLR_HOST=${SOLR_SERVER}&MEMORY=${RAM}&token=${2}"
+curl -u "ksclarke:${1}" -L -s "${JOB_URL}/buildWithParameters?delay=0sec&HOST=${HOST_NAME}&JKS=${JKS_PATH}&SOLR_HOST=${SOLR_SERVER}&MEMORY=${RAM}&token=${2}&GITHUB_BRANCH=${GITHUB_BRANCH}"
 
 # Poll every ten seconds until the build is finished
 while [ $GREP_RETURN_CODE -eq 0 ]
