8e85977a0265c4739120ce3a6fba1d13799dcba7
avu0ng <avuong@library.ucla.edu>
2017-12-13 16:46:39 -0800

    Fix JKS new deployment

--- files/jksgen-incommon.sh
+++ files/jksgen-incommon.sh
@@ -14,23 +14,26 @@ fi
 
 # Delete the JKS from our previous run (if it's still around)
 sudo rm -f le_jiiify.jks
+NEWKEYPASS=$(cat /tmp/supervisord.conf | grep -i key.pass | cut -d '"' -f2 | cut -d '=' -f2 cat /tmp/supervisord.conf | grep -i key.pass | cut -d '"' -f2 | cut -d '=' -f2)
+
+sed -i "s/CHANGEKEYPASS/$NEWKEYPASS/g" /etc/supervisord.conf
 
 SSLROOT="/var/local/ssl/$1"
 
-echo $2
+echo $NEWKEYPASS
 
 sudo openssl pkcs12 -export \
   -in "$SSLROOT/current-crt" \
   -inkey "$SSLROOT/current-key" \
   -certfile "$SSLROOT/current-chain" \
   -out "/tmp/jiiify_cert_and_key.p12" \
-  -password "pass:$2"
+  -password "pass:$NEWKEYPASS"
 
 sudo keytool -importkeystore \
   -srckeystore "/tmp/jiiify_cert_and_key.p12" \
   -srcstoretype "pkcs12" \
-  -srcstorepass "$2" \
+  -srcstorepass "$NEWKEYPASS" \
   -destkeystore "le_jiiify.jks" \
-  -destkeypass "$2" \
-  -deststorepass "$2" \
+  -destkeypass "$NEWKEYPASS" \
+  -deststorepass "$NEWKEYPASS" \
   -trustcacerts
